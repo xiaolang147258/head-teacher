f18cb908d415db33f05fb31554250bbd2fbdb4b4
xiaolang147258 <2443611475@qq.com>
2019-04-15 15:23:42 +0800

    a

--- js/vuex.js
+++ js/vuex.js
@@ -1,9 +1,9 @@
 
 
 
-var Main_domain_name='http://teachers.cieo.com.cn';//主域名http://teachers.cieo.com.cn
+var Main_domain_name='http://teacher-dev47.cieo.com.cn';//主域名http://teachers.cieo.com.cn
 
 //var Domain_name_for_testing='http://teacher-dev.cieo.com.cn';//测试的域名
-
+//Student_list_s:[],
 
 //
