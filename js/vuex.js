73856dfa0853079d78da5917438eeab1ac6c27ab
lidebing <lidebing@qq.com>
2019-10-18 10:29:54 +0800

    tijia

--- js/vuex.js
+++ js/vuex.js
@@ -1,7 +1,9 @@
 
 
 
-var Main_domain_name='http://teacher-dev47.cieo.com.cn';//主域名http://teachers.cieo.com.cn
+var Main_domain_name='http://ae-teacher.cieo.com.cn';//主域名http://teachers.cieo.com.cn
+
+// var Main_domain_name='';
 
 //var Domain_name_for_testing='http://teacher-dev.cieo.com.cn';//测试的域名
 //Student_list_s:[],
